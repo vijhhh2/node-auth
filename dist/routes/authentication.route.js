93512e548c0b48f18230b0e8374829efb00676eb
vijay <vijay.mangapoti@gmail.com>
2024-01-04 15:35:03 +0530

    Updated the validations

--- dist/routes/authentication.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const authRoutes = (0, express_1.Router)();
-authRoutes.post('/register', user_controller_1.registerController);
-authRoutes.post('/login', user_controller_1.loginController);
-authRoutes.post('/loggedIn', user_controller_1.loggedInController);
-exports.default = authRoutes;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,17 +2,33 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { v4 } from "uuid";
+import { z } from "zod";
 
 import { UserLoginBody, UserRegistrationBody } from "../DTOs/user.dto";
 import { User } from "../models/user.model";
 
 let users: User[] = [];
+const userValidation = z.object({
+  username: z.string(),
+  password: z
+    .string()
+    .min(6)
+    .regex(/^[a-zA-Z0-9_]*$/, "Only alpha numeric is allowed"),
+});
 
 export const registerController = async (
   req: Request<null, null, UserRegistrationBody>,
   res: Response
 ) => {
-  const { username, password } = req.body;
+  // Validate the body
+  const data = userValidation.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({
+      message: "Invalid username or password",
+    });
+    return;
+  }
+  const { username, password } = data.data;
   // Hash the password
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, salt);
@@ -43,7 +59,15 @@ export const loginController = async (
   res: Response
 ) => {
   try {
-    const { username, password } = req.body;
+    // Validate the body
+    const data = userValidation.safeParse(req.body);
+    if (!data.success) {
+      res.status(400).json({
+        message: "Invalid username or password",
+      });
+      return;
+    }
+    const { username, password } = data.data;
 
     // check if user exists
     const userIndex = users.findIndex((user) => user.username === username);
@@ -89,17 +113,18 @@ export const loggedInController = (req: Request, res: Response) => {
   }
 
   try {
-    if (token.startsWith('Bearer ')) {
-        // Remove Bearer from string
-        token = token.slice(7, token.length).trimStart();
+    if (token.startsWith("Bearer ")) {
+      // Remove Bearer from string
+      token = token.slice(7, token.length).trimStart();
     }
-    const user = jwt.verify(token,  process.env.ACCESS_TOKEN_SECRET as string) as User; 
+    const user = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as string
+    ) as User;
     res.status(200).json(user);
-
-}
-catch (err) {
+  } catch (err) {
     res.status(400).json({
-        message: "Invalid Token"
+      message: "Invalid Token",
     });
-}
+  }
 };
